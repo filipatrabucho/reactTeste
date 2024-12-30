8e7b715af249728db9c00da4291c8917c4b036dc
Ana Trabucho <98351447+filipatrabucho@users.noreply.github.com>
2024-12-30 09:58:21 +0000

    Test number 2.

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Teste() {
   return (
-    <div>
-        Teste a funcionar o fork.
-    </div>
-
+    <>
+      <div>
+          Teste a funcionar o fork.
+      </div>
+      <div>
+      Teste a funcionar o fork222.
+      </div>
+    </>
   )
 }
 
